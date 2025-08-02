4694b840f890a00d360bce39f61e0e69bcbd4607
m-zamani76 <222499112+m-zamani1998@users.noreply.github.com>
2025-08-02 14:31:49 +0330

    Update taghche analysis.py

--- taghche analysis.py
+++ taghche analysis.py
@@ -1,8 +1,5 @@
 import json
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 
 with open(r"F:\Machine Learning\taghche\taghcheh2.json", "r", encoding="utf-8") as f:
     data = json.load(f)
@@ -21,13 +18,21 @@ print(f"کتابی که بیشترین امتیاز را دارد: {max_rating_r
 max_price_row = df.loc[df["price"].idxmax()]
 print(f"گرون‌ترین کتاب: {max_price_row['title']} با قیمت {max_price_row['price']}")
 
-X = df[["price", "numberOfPages", "authors_count", "tags_count"]]
-y = df["rating"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# from sklearn.linear_model import LinearRegression
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import mean_squared_error
 
-model = LinearRegression().fit(X_train, y_train)
+# X = df[["price", "numberOfPages", "authors_count", "tags_count"]]
+# y = df["rating"]
 
-y_pred = model.predict(X_test)
-rmse = mean_squared_error(y_test, y_pred, squared=False)
-print(f"خطای پیش‌بینی مدل (RMSE): {rmse:.3f}")
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, test_size=0.2, random_state=0
+# )
+
+# model = LinearRegression().fit(X_train, y_train)
+
+# y_pred = model.predict(X_test)
+
+# rmse = mean_squared_error(y_test, y_pred, squared=False)
+# print(f"خطای پیش‌بینی مدل (RMSE): {rmse:.3f}")
